24ccde0f862642318b121e11b2735b6ce1c6bfc5
wbluke <pwb0127@naver.com>
2019-08-08 13:51:20 +0000

    [FIX] deploy.sh 변경

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
+# BLOG_HOME=`~/deploy-practice`
+
+# cd ~/deploy-practice
+# echo cd~~~~
+
 # git pull
-git pull origin wbluke
+git pull
+echo git pull~~~~
 
 # build
 ./gradlew clean build
+echo clean build~~~~
 
 # kill last server
-for p in `sudo lsof -n -i:8080 | grep LISTEN | awk '{print $2}'`; do sudo kill -9 $p; done
+# kill -9 $(lsof -t -i:8000)
+for p in `sudo lsof -n -i:8000 | grep LISTEN | awk '{print $2}'`; do kill -9 $p; done
+echo kill~~~~
 
 # deploy new server
-sudo java -jar ./build/libs/myblog-0.0.1-SNAPSHOT.jar
+java -jar -Dserver.port=8000 ./build/libs/myblog-0.0.1-SNAPSHOT.jar &
+echo deploy success~~~~
